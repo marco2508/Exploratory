d423ca0543ae211183e13e39152a36280721f634
Marco Lamers <62471926+marco2508@users.noreply.github.com>
2020-04-27 09:54:08 +0200

    Add files via upload

---
+++ course things.R
@@ -0,0 +1,10 @@
+#course 4 started 27042020
+#week 1
+#summary( dataframe$column)  <- 6 nummer summary 
+#boxplot col
+#hist col, breaks
+#rug <- shows where the values are...
+#abline <- thresholds
+#barplot col main(title)
+#https://www.r-graph-gallery.com/
+library( swirl )
